aff4de9e5d15034a1c3feb923683b39450f2e2bb
Rudrakshi-Bansal <rudrakshi.bansal.ug23@nsut.ac.in>
2024-07-06 20:13:56 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Projects from "./Projects/Projects";
+import Navbar from './Projects/components/Navbar';
+import Footer from './Projects/components/Footer'
+
+function App() {
+    return (
+      <>
+      <Navbar/>
+      <Projects/>
+      <Footer/>
+      </>
+    )
+  }
+
+
+export default App
